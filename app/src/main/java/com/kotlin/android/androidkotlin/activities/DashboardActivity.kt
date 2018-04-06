9fc9e397c53e0c425e71008929067dd6e06b5613
dilip <dilipmaharjann@gmail.com>
2018-04-06 11:21:26 -0400

    drawable basics

--- app/src/main/java/com/kotlin/android/androidkotlin/activities/DashboardActivity.kt
+++ app/src/main/java/com/kotlin/android/androidkotlin/activities/DashboardActivity.kt
@@ -1,9 +1,11 @@
 package com.kotlin.android.androidkotlin.activities
 
+import android.graphics.drawable.AnimationDrawable
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import com.kotlin.android.androidkotlin.R
+import kotlinx.android.synthetic.main.activity_dashboard.*
 
 class DashboardActivity : AppCompatActivity() {
 
@@ -13,6 +15,11 @@ class DashboardActivity : AppCompatActivity() {
         val email = intent.getStringExtra("email")
         findViewById<TextView>(R.id.mWelcomeTv).apply {
             text = email
+        }
+        dashboard.setBackgroundResource(R.drawable.drawable_animation)
+        val anim:AnimationDrawable=dashboard.background as AnimationDrawable
+        dashboard.setOnClickListener {
+            anim.start()
         }
     }
 }
