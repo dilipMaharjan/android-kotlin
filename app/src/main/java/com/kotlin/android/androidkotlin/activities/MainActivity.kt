f7ca21fd7633b08a2995f3e9807687fe75c958b9
dilip <dilipmaharjann@gmail.com>
2018-04-27 15:54:04 -0400

    diff types of animators

--- app/src/main/java/com/kotlin/android/androidkotlin/activities/MainActivity.kt
+++ app/src/main/java/com/kotlin/android/androidkotlin/activities/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.kotlin.android.androidkotlin.activities
 
 import android.content.Intent
+import android.graphics.drawable.AnimatedVectorDrawable
+import android.graphics.drawable.VectorDrawable
 import android.os.Bundle
+import android.support.graphics.drawable.AnimatedVectorDrawableCompat
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.animation.AlphaAnimation
+import android.view.animation.AnimationUtils
 import com.kotlin.android.androidkotlin.R
 import com.kotlin.android.androidkotlin.kotlin.Authenticate
 import kotlinx.android.synthetic.main.activity_main.*
@@ -22,5 +28,31 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
             }
         }
+        animateView(mLoginBtn)
+        mEmailTv.animate().apply {
+            duration=1000
+            alpha(0.2f)
+            start()
+        }
     }
+
+    //view animator
+    fun animateView(view: View) {
+        animateViewWithCode(view)
+        animateViewWithXml(view)
+    }
+
+    fun animateViewWithCode(view: View) {
+        var alphaAnimation = AlphaAnimation(1f, 0f)
+        alphaAnimation.duration = 5000
+        view.startAnimation(alphaAnimation)
+
+    }
+
+    fun animateViewWithXml(view: View) {
+        val move = AnimationUtils.loadAnimation(this, R.anim.fade_out)
+        move.duration = 10000
+        view.startAnimation(move)
+    }
+
 }
