660f371c08a530bc0dc9f49739d6261c5e7e2152
crazdrms <dilipmaharjann@gmail.com>
2018-05-02 22:42:50 -0400

    designs

---
+++ app/src/main/java/com/kotlin/android/androidkotlin/activities/ConstraintLayoutTryOuts.kt
@@ -0,0 +1,16 @@
+package com.kotlin.android.androidkotlin.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.kotlin.android.androidkotlin.R
+
+/**
+ *
+ *   Created by Dilip Maharjan on 1-05-2018
+ */
+class ConstraintLayoutTryOuts : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.design_tryouts)
+    }
+}
