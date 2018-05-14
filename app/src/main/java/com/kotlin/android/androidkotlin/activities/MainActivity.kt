77389777361650599f189de3d107dedb40a7c784
dilip <dilipmaharjann@gmail.com>
2018-05-14 09:11:39 -0400

    layout props

--- app/src/main/java/com/kotlin/android/androidkotlin/activities/MainActivity.kt
+++ app/src/main/java/com/kotlin/android/androidkotlin/activities/MainActivity.kt
@@ -1,26 +1,53 @@
 package com.kotlin.android.androidkotlin.activities
 
-import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
 import com.kotlin.android.androidkotlin.R
-import com.kotlin.android.androidkotlin.kotlin.Authenticate
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    val TAG = "MAINACTIVITY"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mLoginBtn.setOnClickListener {
-            val username = mUsernameTv.text.toString()
-            val auth = Authenticate().login(username.trim(), mPasswordTv.text.toString().trim())
-            if (auth) {
-                val intent = Intent(this, DashboardActivity::class.java).apply {
-                    putExtra("username", username)
-                }
-                startActivity(intent)
-            }
+
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            getViewBounds(tv)
+            getMeasureWidthAndHeight(tv)
+            getWidthAndHeight(tv)
+            getPadding(tv)
         }
     }
+    fun getViewBounds(view: View) {
+        Log.i(TAG, "left-coordinate ${view.left}")
+        Log.i(TAG, "top-coordinate ${view.top}")
+        Log.i(TAG, "right-coordinate ${view.right}")
+        Log.i(TAG, "bottom-coordinate ${view.bottom}")
+    }
+
+    fun getMeasureWidthAndHeight(view: View) {
+        Log.i(TAG, "measured width  ${view.measuredWidth}")
+        Log.i(TAG, "measured height  ${view.measuredHeight}")
+    }
+
+    fun getWidthAndHeight(view: View) {
+        Log.i(TAG, "width ${view.width}")
+        Log.i(TAG, "height ${view.height}")
+    }
+
+    fun getPadding(view: View) {
+        Log.i(TAG, "paddingLeft ${view.paddingLeft}")
+        Log.i(TAG, "paddingRight ${view.paddingRight}")
+        Log.i(TAG, "paddingTop ${view.paddingTop}")
+        Log.i(TAG, "paddingBottom ${view.paddingBottom}")
+    }
+
 }
